62d03b2eecd941706da5cdb8ac1ea70c408b3dcb
FianRifkySaputra <fiansaputra100@gmail.com>
2023-05-22 12:21:56 +0700

    up

--- login.php
+++ login.php
@@ -1,94 +1,91 @@
 <?php
-//menyertakan file program koneksi.php pada register
-require('koneksi.php');
-//inisialisasi session
 session_start();
-$error = '';
-$validate = '';
-//mengecek apakah sesssion username tersedia atau tidak jika tersedia maka akan diredirect ke halaman index
-if( isset($_SESSION['username']) ) header('Location: index.php');
-//mengecek apakah form disubmit atau tidak
-if( isset($_POST['submit']) ){
-        
-        // menghilangkan backshlases
-        $username = stripslashes($_POST['username']);
-        //cara sederhana mengamankan dari sql injection
-        $username = mysqli_real_escape_string($con, $username);
-         // menghilangkan backshlases
-        $password = stripslashes($_POST['password']);
-         //cara sederhana mengamankan dari sql injection
-        $password = mysqli_real_escape_string($con, $password);
-       
-        //cek apakah nilai yang diinputkan pada form ada yang kosong atau tidak
-        if(!empty(trim($username)) && !empty(trim($password))){
-            //select data berdasarkan username dari database
-            $query      = "SELECT * FROM users WHERE username = '$username'";
-            $result     = mysqli_query($con, $query);
-            $rows       = mysqli_num_rows($result);
-            if ($rows != 0) {
-                $hash   = mysqli_fetch_assoc($result)['password'];
-                if(password_verify($password, $hash)){
-                    $_SESSION['username'] = $username;
-               
-                    header('Location: index.php');
-                }
-                            
-            //jika gagal maka akan menampilkan pesan error
-            } else {
-                $error =  'Register User Gagal !!';
-            }
-            
-        }else {
-            $error =  'Data tidak boleh kosong !!';
-        }
-    } 
+include_once('koneksi.php');
+$database = new database();
+
+if (isset($_SESSION['is_login'])) {
+  header('location:index.php');
+}
+
+if (isset($_COOKIE['username'])) {
+  $database->relogin($_COOKIE['username']);
+  header('location:index.php');
+}
+
+if (isset($_POST['login'])) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  if (isset($_POST['remember'])) {
+    $remember = TRUE;
+  } else {
+    $remember = FALSE;
+  }
+
+  if ($database->login($username, $password, $remember)) {
+    header('location:index.php');
+  }
+}
+
+if (isset($_GET["error"])) {
+  $error = $_GET["error"];
+} else {
+  $error = "";
+}
+$message = "";
+if ($error == "failed") {
+  $message = "Failed to login, please login again";
+}
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-<!-- meta tags -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
- 
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<!-- costum css -->
-<link rel="stylesheet" href="style.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Jekyll v3.8.5">
+  <title>Login Form</title>
+
+  <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sign-in/">
+
+  <!-- Bootstrap core CSS -->
+
+  <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+
+  <style>
+    .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+    }
+
+    @media (min-width: 768px) {
+      .bd-placeholder-img-lg {
+        font-size: 3.5rem;
+      }
+    }
+  </style>
+  <!-- Custom styles for this template -->
+  <link href="signin.css" rel="stylesheet">
+
 </head>
-<body>
-        <section class="container-fluid mb-4">
-            <!-- justify-content-center untuk mengatur posisi form agar berada di tengah-tengah -->
-            <section class="row justify-content-center">
-            <section class="col-12 col-sm-6 col-md-4">
-                <form class="form-container" action="login.php" method="POST">
-                    <h4 class="text-center font-weight-bold"> Sign-In </h4>
-                    <?php if($error != ''){ ?>
-                        <div class="alert alert-danger" role="alert"><?= $error; ?></div>
-                    <?php } ?>
-                   
-                    <div class="form-group">
-                        <label for="username">Username</label>
-                        <input type="text" class="form-control" id="username" name="username" placeholder="Masukkan username">
-                    </div>
-                    <div class="form-group">
-                        <label for="InputPassword">Password</label>
-                        <input type="password" class="form-control" id="InputPassword" name="password" placeholder="Password">
-                        <?php if($validate != '') {?>
-                            <p class="text-danger"><?= $validate; ?></p>
-                        <?php }?>
-                    </div>
-                 
-                    <button type="submit" name="submit" class="btn btn-primary btn-block">Sign In</button>
-                    <div class="form-footer mt-2">
-                        <p> Belum punya account? <a href="register.php">Register</a></p>
-                    </div>
-                </form>
-            </section>
-            </section>
-        </section>
-    <!-- Bootstrap requirement jQuery pada posisi pertama, kemudian Popper.js, dan  yang terakhit Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+<body class="text-center">
+  <form class="form-signin" method="post" action="">
+
+    <h1 class="h3 mb-3 font-weight-normal">Silahkan Login</h1>
+    <label for="username" class="sr-only">Username</label>
+    <input type="text" id="username" class="form-control" placeholder="Username" name="username" required autofocus>
+    <label for="password" class="sr-only">Password</label>
+    <input type="password" id="password" class="form-control" placeholder="Password" name="password" required>
+
+    <button class="btn btn-lg btn-primary btn-block" type="submit" name="login">Sign in</button>
+    <a href="register.php" class="btn btn-lg btn-success btn-block">Register</a>
+  </form>
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -1,124 +1,111 @@
 <?php
-//menyertakan file program koneksi.php pada register
-require('koneksi.php');
-//inisialisasi session
-session_start();
-$error = '';
-$validate = '';
-//mengecek apakah form registrasi di submit atau tidak
-if( isset($_POST['submit']) ){
-        // menghilangkan backshlases
-        $username = stripslashes($_POST['username']);
-        //cara sederhana mengamankan dari sql injection
-        $username = mysqli_real_escape_string($con, $username);
-        $name     = stripslashes($_POST['name']);
-        $name     = mysqli_real_escape_string($con, $name);
-        $email    = stripslashes($_POST['email']);
-        $nowhatsapp     = stripslashes($_POST['nowhatsapp']);
-        $nowhatsapp     = mysqli_real_escape_string($con, $nowhatsapp);
-        $email    = mysqli_real_escape_string($con, $email);
-        $password = stripslashes($_POST['password']);
-        $password = mysqli_real_escape_string($con, $password);
-        $repass   = stripslashes($_POST['repassword']);
-        $repass   = mysqli_real_escape_string($con, $repass);
-        //cek apakah nilai yang diinputkan pada form ada yang kosong atau tidak
-        if(!empty(trim($name)) && !empty(trim($username)) && !empty(trim($email))&& !empty(trim($nowhatsapp)) && !empty(trim($password)) && !empty(trim($repass))){
-            //mengecek apakah password yang diinputkan sama dengan re-password yang diinputkan kembali
-            if($password == $repass){
-                //memanggil method cek_nama untuk mengecek apakah user sudah terdaftar atau belum
-                if( cek_nama($name,$con) == 0 ){
-                    //hashing password sebelum disimpan didatabase
-                    $pass  = password_hash($password, PASSWORD_DEFAULT);
-                    //insert data ke database
-                    $query = "INSERT INTO users (username,name,email, password ) VALUES ('$username','$nama','$email','$nowhatsapp','$pass')";
-                    $result   = mysqli_query($con, $query);
-                    //jika insert data berhasil maka akan diredirect ke halaman index.php serta menyimpan data username ke session
-                    if ($result) {
-                        $_SESSION['username'] = $username;
-                       
-                        header('Location: login.php');
-                    
-                    //jika gagal maka akan menampilkan pesan error
-                    } else {
-                        $error =  'Register User Gagal !!';
-                    }
-                }else{
-                        $error =  'Username sudah terdaftar !!';
-                }
-            }else{
-                $validate = 'Password tidak sama !!';
-            }
-            
-        }else {
-            $error =  'Data tidak boleh kosong !!';
-        }
-    } 
-    //fungsi untuk mengecek username apakah sudah terdaftar atau belum
-    function cek_nama($username,$con){
-        $nama = mysqli_real_escape_string($con, $username);
-        $query = "SELECT * FROM users WHERE username = '$nama'";
-        if( $result = mysqli_query($con, $query) ) return mysqli_num_rows($result);
-    }
+$koneksi = mysqli_connect("localhost","root","","pemesanan_tiket_liburan");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<!-- meta tags -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<!-- costum css -->
-<link rel="stylesheet" href="style.css">
-</head>
-<body>
-        <section class="container-fluid mb-4">
-            <!-- justify-content-center untuk mengatur posisi form agar berada di tengah-tengah -->
-            <section class="row justify-content-center">
-            <section class="col-12 col-sm-6 col-md-4">
-                <form class="form-container" action="register.php" method="POST">
-                    <h4 class="text-center font-weight-bold"> Sign-Up </h4>
-                    <?php if($error != ''){ ?>
-                        <div class="alert alert-danger" role="alert"><?= $error; ?></div>
-                    <?php } ?>
-                   
-                    <div class="form-group">
-                        <label for="name">Nama</label>
-                        <input type="text" class="form-control" id="name" name="name" placeholder="Masukkan Nama">
-                    </div>
-                    <div class="form-group">
-                        <label for="InputEmail">Alamat Email</label>
-                        <input type="email" class="form-control" id="InputEmail" name="email" aria-describeby="emailHelp" placeholder="Masukkan email">
-                    </div>
-                    <div class="form-group">
-                        <label for="username">Username</label>
-                        <input type="text" class="form-control" id="username" name="username" placeholder="Masukkan username">
-                    </div>
-                    <div class="form-group">
-                        <label for="InputPassword">Password</label>
-                        <input type="password" class="form-control" id="InputPassword" name="password" placeholder="Password">
-                        <?php if($validate != '') {?>
-                            <p class="text-danger"><?= $validate; ?></p>
-                        <?php }?>
-                    </div>
-                    <div class="form-group">
-                        <label for="InputPassword">Re-Password</label>
-                        <input type="password" class="form-control" id="InputRePassword" name="repassword" placeholder="Re-Password">
-                        <?php if($validate != '') {?>
-                            <p class="text-danger"><?= $validate; ?></p>
-                        <?php }?>
-                    </div>
-                    <button type="submit" name="submit" class="btn btn-primary btn-block">Register</button>
-                    <div class="form-footer mt-2">
-                        <p> Sudah punya account? <a href="login.php">Login</a></p>
-                    </div>
-                </form>
-            </section>
-            </section>
-        </section>
-    <!-- Bootstrap requirement jQuery pada posisi pertama, kemudian Popper.js, dan  yang terakhit Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<?php 
+include_once('koneksi.php');
+$database = new database();
+if(isset($_POST['register']))
+{
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'],PASSWORD_DEFAULT);
+    $nama = $_POST['nama'];
+    if($database->register($username,$password,$nama))
+    {
+      header('location:login.php');
+    }
+}
+ 
+?>
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Register Form</title>
+ 
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sticky-footer/">
+ 
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+
+ 
+ 
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+ 
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="sticky-footer.css" rel="stylesheet">
+  </head>
+  <body class="d-flex flex-column h-100">
+    <!-- Begin page content -->
+<main role="main" class="flex-shrink-0">
+  <div class="container">
+    <h1 class="mt-5">Register Form</h1>
+    <p class="lead">Silahkan Daftarkan Identitas Anda</p>
+    <hr/>
+    <form method="post" action="">
+    <div class="form-group row">
+      <label for="username" class="col-sm-2 col-form-label">Username</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+      </div>
+    </div>
+      <br>
+    <div class="form-group row">
+      <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama">
+      </div>
+    </div>
+ <br>
+    <div class="form-group row">
+      <label for="Email" class="col-sm-2 col-form-label">Email</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="Email" name="Email" placeholder="Email">
+      </div>
+    </div>
+<br>
+    <div class="form-group row">
+      <label for="Nomor Telepon" class="col-sm-2 col-form-label">Nomor Telepon</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="Nomor Telepon" name="Nomor Telepon" placeholder="Nomor Telepon">
+      </div>
+    </div>
+<br>
+  <div class="form-group row">
+    <label for="password" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+    </div>
+  </div>
+  <br>
+  <div class="form-group row">
+    <div class="col-sm-10">
+      <a href="login.php" class="btn btn-success">Login</a>
+      <button type="submit" class="btn btn-primary" name="register">Register</button>
+    </div>
+  </div>
+</form>
+  </div>
+</main>
+ 
+
 </body>
 </html>
+
